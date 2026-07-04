652fa348967fbb6db991a98b8b883f468f0522e4
N DIVIJ <ndivij2004@gmail.com>
2026-07-04 14:43:15 +0000

    chore: remove generated slack ui outputs

--- packages/slack-ui/src/index.d.ts
+++
@@ -1,6 +0,0 @@
-import type { ReviewPacket } from "@securelore/review-core";
-export interface SlackBlock {
-    type: string;
-    [key: string]: unknown;
-}
-export declare function renderReviewPacket(packet: ReviewPacket): SlackBlock[];
